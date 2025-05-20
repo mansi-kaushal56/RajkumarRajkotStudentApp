ce51a56f241be8e6f63570870adbb88e8ef3d501
Mansi Kaushal <kaushal.mansi.tdv@gmail.com>
2025-05-20 11:20:22 +0530

    final commit

---
+++ HAPSStudentApp/Dashboard/Controllers/ChangePasswordVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChangePasswordVC.swift
+//  HAPSStudentApp
+//
+//  Created by Raj Mohan on 19/05/25.
+//
+
+import UIKit
+
+class ChangePasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
